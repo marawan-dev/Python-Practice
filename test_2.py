ce42e5646f13074f9de21d15b7644a8f159b7d68
marawan-dev <student.sch20l@email.com>
2026-05-24 15:47:19 +0400

    Math logic

--- test_2.py
+++ test_2.py
@@ -46,3 +46,51 @@ print(boarding.count('e'))
 #.capitalize() just makes the first index capital
 #.title() capitalizies the first letter of each word
 #.isupper(), returns true if all letters are lowercase
+
+print('Calculations:')
+
+int_1 = 24
+int_2 = 5
+
+# the % is the mod
+remainder = int_1 % int_2
+
+# floor division is //
+years = int_1 // int_2
+
+# Exponentiation is **
+rate = int_1 ** int_2
+
+print(remainder, type(remainder))
+print(years)
+print(rate)
+
+remainder = float(remainder)
+print(type(remainder))
+# int does the same thing
+
+float_1 = 8.3
+float_2 = -2.1
+
+multiply_float = float_1 * float_2
+print(round(abs(multiply_float), 2))
+# round(var, d.p.)
+
+#combined function pow(base, power, mod)
+print(pow(2,3,5))
+print(pow(2,3))
+
+#argumented operations
+int_1 += int_2
+print(int_1)
+int_2 -= int_1
+print(int_2)
+
+#text operations
+intro1 = 'Hello '
+intro2 = 'Internet'
+# *= repeats the text
+intro1 *= 2
+intro1 += intro2
+print(intro1)
+
