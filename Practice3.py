b63375fd17e9173eae4de4c1ee453a959f809334
marawan-dev <student.sch20l@email.com>
2026-06-05 07:15:49 +0400

    learned lists 1

--- Practice3.py
+++ Practice3.py
@@ -1,5 +1,6 @@
 ## Functions
 # input(), optional text inside
+# use help(str) for functions
 
 #baby yoda meme
 def bym():
@@ -13,3 +14,30 @@ def add(x, y):
 sum = add(5,10)
 print(sum)
 
+trans_table = str.maketrans('1','2')
+text = 'update 1'
+result = text.translate(trans_table)
+print(result)
+
+## Lists
+travel_plan = ['Russia', 'USA', 'Germany', 'Turkey']
+print(travel_plan[-2])
+
+letters = list(travel_plan[1])
+print(letters)
+
+travel_plan[-1] = 'Canada'
+print(travel_plan)
+
+del travel_plan[1]
+print(travel_plan)
+
+print('Canada' in travel_plan)
+
+to_do = ['Study', ['Computer science', 'Math', 'Physics'], 'Pack my things']
+print(to_do[1][0])
+
+studying, material, *rest = to_do
+print(studying)
+print(material)
+print(rest)
