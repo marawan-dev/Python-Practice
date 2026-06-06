7af1f1700fbee6b11981d2996d18f527218ab12d
marawan-dev <student.sch20l@email.com>
2026-06-06 17:40:53 +0400

    learned lists 2 and tuples

--- Practice3.py
+++ Practice3.py
@@ -41,3 +41,42 @@ studying, material, *rest = to_do
 print(studying)
 print(material)
 print(rest)
+
+## lists part 2
+numbers = [1,2,3,4,5]
+more_numbers = [6,7,8,9]
+numbers.append(more_numbers) # embeds
+print(numbers)
+
+numbers.remove(more_numbers)
+numbers.extend(more_numbers) # adds
+print(numbers)
+
+numbers.insert(2,2.5)
+print(numbers)
+
+numbers.pop(2) # removes the element in the index
+print(numbers)
+
+numbers.clear()
+print(numbers)
+
+random_numbers = [11,9,3,5,43]
+sorted_numbers = sorted(random_numbers) # sorted() works for both lists and tuples
+print(sorted_numbers)
+
+random_numbers.sort()
+print(random_numbers)
+
+sorted_numbers.reverse()
+print(sorted_numbers)
+
+print(sorted_numbers.index(3))
+
+## tuples
+group_names = ('Alex', 'Steve', 'Adam', 'Eve')
+group_ages = (15, 17, 16, 16)
+print(group_ages.count(16))
+
+sorted_group_names = sorted(group_names, key=len)
+print(sorted_group_names)
